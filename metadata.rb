809c99fba29cb96532a9bf83574059db1a288c74
btidwell <btidwell@users.noreply.github.com>
2015-08-04 15:22:25 -0400

    remove repo source

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,4 @@
 name             'nodejs-wrapper-opsworks'
-license          'All rights reserved'
 description      'Installs/Configures nodejs-0.12.0'
 long_description 'Installs/Configures nodejs-0.12.0'
 version          '0.1.0'
